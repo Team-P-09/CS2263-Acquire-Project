3a53e54af3487640ff69cfeda9438bf3155844e1
ThomasEvans <Thomasrulonevans@gmail.com>
2021-11-07 18:58:35 -0700

    Added tests for Tile, TileStack, and Gameboard

---
+++ app/src/test/java/edu/isu/cs2263/CS2263_Acquire_Project/TestTileStack.java
@@ -0,0 +1,19 @@
+package edu.isu.cs2263.CS2263_Acquire_Project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTileStack {
+    @BeforeEach
+    void setUp(){
+        TileStack testingTileStack = new TileStack();
+        testingTileStack.initializeTiles();
+    }
+
+    @Test
+    void testPopTile(){
+        assertNotNull(testingTileStack.popTile());
+    }
+
+}
