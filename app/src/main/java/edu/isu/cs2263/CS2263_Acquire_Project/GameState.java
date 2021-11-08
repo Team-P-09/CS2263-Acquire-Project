e1156157a75ecd516c64c6619ec566c18b0e08c0
unknown <David Lindeman>
2021-11-07 19:13:41 -0700

    created corporations feature branch

---
+++ app/src/main/java/edu/isu/cs2263/CS2263_Acquire_Project/GameState.java
@@ -0,0 +1,50 @@
+package edu.isu.cs2263.CS2263_Acquire_Project;
+
+public class GameState {
+    //Gameboard gameboard;
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public void startGame(){
+        System.out.println("Start Game");
+    }
+
+    public void endGame(){
+        System.out.println("End Game");
+    }
+
+    public void saveGame() { System.out.println("Savegame");}
+
+    public void loadGame() { System.out.println("Loadgame");}
+
+    public void initGame(int NumbPlayers){
+        System.out.println("initGame");
+    }
+
+    public void initScoreboard(){
+        System.out.println("initScoreboard");
+    }
+
+    public void initPlayers(){
+        System.out.println("initPlayers");
+    }
+
+    public void initTileStack(){
+        System.out.println("InitTStack");
+    }
+
+    public void displayGameboard(){
+        System.out.println("DisplayGameboard");
+    }
+
+    public void displayScoreboard(){
+        System.out.println("displayScoreboard");
+    }
+
+    public void displayHand(){
+        System.out.println("Displayhand");
+    }
+}
