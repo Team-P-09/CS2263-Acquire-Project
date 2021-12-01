a58fafa9b1f704770f789d4a916aaf7b76dcd8f1
ThomasRulonEvans <thomasrulonevans@gmail.com>
2021-12-01 15:18:08 -0700

    Fixed double call to render in UI controller that caused a Nullpointexception.

--- app/src/main/java/edu/isu/cs2263/CS2263_Acquire_Project/UIController.java
+++ app/src/main/java/edu/isu/cs2263/CS2263_Acquire_Project/UIController.java
@@ -56,7 +56,6 @@ public class UIController {
         GameState gameState = GameState.getInstance(null);
 
         //updates the current players hand
-        gameState.checkPlayerHandForRefresh(gameState.getCurrentPlayer().getPName());
         PlayerInfo playerInfo = gameState.getCurrentPlayer();
         Text playerLabel = (Text) scene.lookup("#turnLabel");
         playerLabel.setText(playerInfo.getPName());
@@ -272,13 +271,14 @@ public class UIController {
             //tiles must be removed at the start of the players turn else a tile can be played the turn after it becomes unplayable
             currentPlayer = gameState.getCurrentPlayer();
             gameState.checkPlayerHandForRefresh(currentPlayer.getPName());
+
+            gameState.nextPlayer();
+            currentPlayer = gameState.getCurrentPlayer();
+            gameState.checkPlayerHandForRefresh(currentPlayer.getPName());
+
+            gameState.resetBuyCounter();
             render(event);
         }
-
-//        gameState.checkPlayerHandForRefresh(gameState.getCurrentPlayer().getPName());
-        gameState.nextPlayer();
-        gameState.resetBuyCounter();
-        render(event);
     }
 
 
